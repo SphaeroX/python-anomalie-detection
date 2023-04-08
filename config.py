d0c31ee5bdb8e7f4425a9f01a11deb857fd4a610
SphaeroX <SphaeroX@gmail.com>
2023-04-08 16:20:30 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,9 @@
+# Einstellbare Variablen
+use_contour = False
+resize_x = 224
+resize_y = 224
+calibration_percent = 1.06
+video_path = 'soll_zustand.avi'
+min_recorded_frames = 20
+contour_threshold = 5
+recording_duration = 20
