460fb07af11b3d54e4702a002e41b3ffcd6d346a
SphaeroX <SphaeroX@gmail.com>
2023-04-10 14:07:16 +0200

    .

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
-# Einstellbare Variablen
-use_contour = False
+# Allgemein
 resize_x = 224
 resize_y = 224
-calibration_percent = 1.06
+
+# Recording
 video_path = 'soll_zustand.avi'
-min_recorded_frames = 20
-contour_threshold = 5
 recording_duration = 20
+
+# Auslöser für Detection
+min_recorded_frames = 20
+calibration_percent = 1.06
+
+#Konturen
+use_contour = False
+contour_threshold = 5
